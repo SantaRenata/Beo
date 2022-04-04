1780d824b6a7ce1657a4344788f5170304c20ea8
SantaRenata <renatasanta2002@gmail.com>
2022-04-04 11:27:16 +0200

    PHP nyelv konverzioi

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php
-echo file_get_contents("templates/head.tpl");
-echo file_get_contents("templates/nav.tpl");
-echo file_get_contents("templates/home.tpl");
-echo file_get_contents("templates/foot.tpl");
-//$data = file_get_contents("templates/head.tpl");
-//echo $data;
+$szam = 35;
+echo "<!DOCTYPE html>\n";
+echo "<html>";
+echo "<body>";
+
+echo "alag\nsor<br>";
+echo "alag<br>sor";
+echo "<br>Tipus: ".gettype($szam);
+
+echo <<<Kiskutya
+<ul>
+    <li>alma</li>
+    <li>körte</li>
+</ul>
+Kiskutya;
+
+echo "<br>PHP_OS: ".PHP_OS;
+const MIN_KOR=18;
+echo "<br>Jöhet: ".MIN_KOR;
+
+printf("<br>|%20d|",58);
+
+//Matematikai függvények
+echo "<br>";
+echo sqrt(9);
+
+echo "<br>";
+$szog=1;
+echo sin($szog*pi()/180);
+
+echo "<br>";
+echo pow(2,8);
+
+echo "<br>";
+echo rand(1,6);
+
+echo "<br>";
+$szamStr = "35";
+echo $szamStr+2;
+
+echo "<br>";
+
+
+echo "</body></html>";
 ?>
